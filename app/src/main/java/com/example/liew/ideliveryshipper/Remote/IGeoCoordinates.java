bb897b237b72b27204e6a847bbba4c8437007d7b
Liew Syet Chau <syetchau@hotmail.com>
2019-11-04 21:57:19 +0800

    Update IGeoCoordinates.java

--- app/src/main/java/com/example/liew/ideliveryshipper/Remote/IGeoCoordinates.java
+++ app/src/main/java/com/example/liew/ideliveryshipper/Remote/IGeoCoordinates.java
@@ -5,10 +5,10 @@ package com.example.liew.ideliveryshipper.Remote;
         import retrofit2.http.Query;
 
 public interface IGeoCoordinates {
-    @GET("maps/api/geocode/json?key=AIzaSyBW3rhW1EhjhW36DmMyoTTBup4E6Gu1LCY&sensor=true&language=en")
+    @GET("maps/api/geocode/json?key=yourkey&sensor=true&language=en")
     Call<String> getGeoCode(@Query("address") String address);
 
-    @GET("maps/api/directions/json?key=AIzaSyBW3rhW1EhjhW36DmMyoTTBup4E6Gu1LCY&sensor=true&language=en&mode=driving")
+    @GET("maps/api/directions/json?key=yourkey&sensor=true&language=en&mode=driving")
     Call<String> getDirections(@Query("origin") String origin, @Query("destination") String destination);
 
 }
